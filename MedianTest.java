628b1395d029a6e5bd6f79e31aa71306b9719067
rubeea <rubeea@users.noreply.github.com>
2017-03-26 20:21:03 +0500

    Java class for Median

---
+++ MedianTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class MedianTest{
+	
+	static int commandcount=0,loopcounter=0,element=0;
+
+	public static void main(String[] args){
+
+		Scanner scan=new Scanner(System.in);
+		commandcount= scan.nextInt();
+		Median m= new Median(); //initialize median class
+
+		do{
+
+			loopcounter++;
+			Scanner scann=new Scanner(System.in);
+			element= scann.nextInt(); //input element for computing median
+			m.addElement(element);
+
+		}while(loopcounter!=commandcount);
+
+	}
+}
